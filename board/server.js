11a5853e575bc22b485697f49077d3abde483041
aiegoo <eozz21@gmail.com>
2019-03-06 17:50:16 +0900

    add expess files to initiate with update on the ass. files

---
+++ board/server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var fs = require("fs")
+
+app.set('views', 'ejs');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(session({
+ secret: '@#@$MYSIGN#@$#$',
+ resave: false,
+ saveUninitialized: true
+}));
+
+
+var router = require('./router/main')(app, fs);
